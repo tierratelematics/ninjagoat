e121c434a9eb025acffbecfd8c5ba03802653c59
Mattia Franchetto <mt.franchetto@gmail.com>
2016-05-04 12:53:26 +0200

    Refactor http headers append

--- scripts/net/HttpClient.ts
+++ scripts/net/HttpClient.ts
@@ -2,35 +2,38 @@ import IHttpClient from "./IHttpClient";
 import * as Rx from "rx";
 import "whatwg-fetch";
 import HttpResponse from "./HttpResponse";
+import HttpHeaders from "./HttpHeaders";
 
 class HttpClient implements IHttpClient {
-
-    get(url:string, headers?:{}):Rx.IObservable<HttpResponse> {
-        return this.performNetworkCall(url, 'get');
+    
+    get(url:string, headers?:HttpHeaders):Rx.IObservable<HttpResponse> {
+        return this.performNetworkCall(url, 'get', null, headers);
     }
 
-    post(url:string, body:any, headers?:{}):Rx.IObservable<HttpResponse> {
-        return this.performNetworkCall(url, 'post', body);
+    post(url:string, body:{}|FormData, headers?:HttpHeaders):Rx.IObservable<HttpResponse> {
+        if (!(body instanceof FormData))
+            body = JSON.stringify(body);
+        return this.performNetworkCall(url, 'post', body, headers);
     }
 
-    put(url:string, body:any, headers?:{}):Rx.IObservable<HttpResponse> {
-        return this.performNetworkCall(url, 'put', body);
+    put(url:string, body:{}, headers?:HttpHeaders):Rx.IObservable<HttpResponse> {
+        return this.performNetworkCall(url, 'put', JSON.stringify(body), headers);
     }
 
-    delete(url:string, headers?:{}):Rx.IObservable<HttpResponse> {
-        return this.performNetworkCall(url, 'delete');
+    delete(url:string, headers?:HttpHeaders):Rx.IObservable<HttpResponse> {
+        return this.performNetworkCall(url, 'delete', null, headers);
     }
 
-    private performNetworkCall(url:string, method:string, body?:any):Rx.IObservable<HttpResponse> {
-        let networkCall = window
-            .fetch(url, {
+    private performNetworkCall(url:string, method:string, body?:any, headers?:HttpHeaders):Rx.IObservable<HttpResponse> {
+        return Rx.Observable.fromPromise(
+            window.fetch(url, {
                 method: method,
-                body: body
-            })
-            .then(response => {
+                body: body,
+                headers: headers
+            }).then(response => {
                 return response.json().then(json => new HttpResponse(json, response.headers));
-            });
-        return Rx.Observable.fromPromise(networkCall);
+            })
+        );
     }
 }
 
--- scripts/net/IHttpClient.ts
+++ scripts/net/IHttpClient.ts
@@ -1,11 +1,12 @@
 import * as Rx from "rx";
 import HttpResponse from "./HttpResponse";
+import HttpHeaders from "./HttpHeaders";
 
 interface IHttpClient {
-    get(url:string, headers?:{}):Rx.IObservable<HttpResponse>
-    post(url:string, body:any, headers?:{}):Rx.IObservable<HttpResponse>
-    put(url:string, body:any, headers?:{}):Rx.IObservable<HttpResponse>
-    delete(url:string, headers?:{}):Rx.IObservable<HttpResponse>
+    get(url:string, headers?:HttpHeaders):Rx.IObservable<HttpResponse>
+    post(url:string, body:{}|FormData, headers?:HttpHeaders):Rx.IObservable<HttpResponse>
+    put(url:string, body:{}, headers?:HttpHeaders):Rx.IObservable<HttpResponse>
+    delete(url:string, headers?:HttpHeaders):Rx.IObservable<HttpResponse>
 }
 
 export default IHttpClient
