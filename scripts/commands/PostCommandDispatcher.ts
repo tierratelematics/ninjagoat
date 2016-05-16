db7ed7091d4b7e3eabb54a44eb53e7e7b0bdb45c
Mattia Franchetto <mt.franchetto@gmail.com>
2016-05-16 17:46:20 +0200

    Implement post command dispatcher

---
+++ scripts/commands/PostCommandDispatcher.ts
@@ -0,0 +1,26 @@
+import CommandDispatcher from "./CommandDispatcher";
+import Command from "./Command";
+import CommandEnvelope from "./CommandEnvelope";
+import CommandResponse from "./CommandResponse";
+import IDateRetriever from "../util/IDateRetriever";
+import IGUIDGenerator from "../util/IGUIDGenerator";
+import {HTTP_Post} from "../constants/Transport";
+import IHttpClient from "../net/IHttpClient";
+
+class PostCommandDispatcher extends CommandDispatcher {
+
+    constructor(dateRetriever:IDateRetriever, guidGenerator:IGUIDGenerator, private httpClient:IHttpClient) {
+        super(dateRetriever, guidGenerator);
+    }
+
+    canExecuteCommand(command:Command) {
+        return this.transport === HTTP_Post;
+    }
+
+    executeCommand<T extends Command>(command:CommandEnvelope<T>):Rx.Observable<CommandResponse> {
+        return this.httpClient.post(this.endpoint, command);
+    }
+
+}
+
+export default PostCommandDispatcher
--- test/commands/PostCommandDispatcherSpec.ts
+++ test/commands/PostCommandDispatcherSpec.ts
@@ -4,10 +4,40 @@ import "bluebird";
 import expect = require("expect.js");
 import Rx = require("rx");
 import sinon = require("sinon");
+import PostCommandDispatcher from "../../scripts/commands/PostCommandDispatcher";
+import HttpClient from "../../scripts/net/HttpClient";
+import SinonSandbox = Sinon.SinonSandbox;
+import IHttpClient from "../../scripts/net/IHttpClient";
+import SinonStub = Sinon.SinonStub;
+import {EndpointCommand} from "../fixtures/commands/MockCommands";
+import CommandEnvelope from "../../scripts/commands/CommandEnvelope";
+import MockDateRetriever from "../fixtures/MockDateRetriever";
+import MockGuidGenerator from "../fixtures/MockGuidGenerator";
 
 describe("PostCommandDispatcher, given a command", () => {
 
-    beforeEach(() => {
+    let subject:PostCommandDispatcher,
+        httpClient:IHttpClient,
+        postStub:SinonStub;
 
+    beforeEach(() => {
+        httpClient = new HttpClient();
+        subject = new PostCommandDispatcher(new MockDateRetriever(), new MockGuidGenerator(), httpClient);
+        postStub = sinon.stub(httpClient, "post", () => Rx.Observable.just({response: null}));
+    });
+
+    afterEach(() => {
+        postStub.restore();
+    });
+
+    context("when the transport to be used is an HTTP Post", () => {
+        it("should send it using an http client", () => {
+            subject.dispatch(new EndpointCommand(), {});
+            let envelope = CommandEnvelope.of(new EndpointCommand(), {});
+            envelope.type = "EndpointCommand";
+            envelope.createdTimestamp = "2016-05-16T09:52:18Z";
+            envelope.id = "42";
+            expect(postStub.calledWith("/foo", envelope)).to.be(true);
+        });
     });
 });
