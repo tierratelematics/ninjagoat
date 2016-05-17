fc03ee6751df0f5ae0a92523620766c877cb63e7
Mattia Franchetto <mt.franchetto@gmail.com>
2016-05-17 15:39:04 +0200

    Statically registered socket io client

--- test/model/NotificationManagerSpec.ts
+++ test/model/NotificationManagerSpec.ts
@@ -16,7 +16,7 @@ describe("NotificationManager, given an area and a viewmodel id", () => {
 
     beforeEach(() => {
         client = new MockSocketClient();
-        subject = new NotificationManager(() => Promise.resolve(client));
+        subject = new NotificationManager(client);
         emitStub = sinon.stub(client, "emit");
         notificationsStreamStub = sinon.stub(subject, "getNotificationStream", () => {
             return Rx.Observable.just({url: 'test'});
@@ -29,42 +29,37 @@ describe("NotificationManager, given an area and a viewmodel id", () => {
     });
 
     context("when this viewmodel needs notifications about the model change", () => {
-        it("should subscribe to the backend", (done) => {
-            subject.notificationsFor(new ViewModelContext("Admin", "FakePage")).subscribe(_ => {
-                expect(emitStub.calledWith('subscribe', {
-                    area: "Admin",
-                    viewmodelId: "FakePage",
-                    parameters: undefined
-                })).to.be(true);
-                done();
-            });
+        it("should subscribe to the backend", () => {
+            subject.notificationsFor(new ViewModelContext("Admin", "FakePage"));
+            expect(emitStub.calledWith('subscribe', {
+                area: "Admin",
+                viewmodelId: "FakePage",
+                parameters: undefined
+            })).to.be(true);
         });
 
         context("and custom parameters are needed on the backend side", () => {
-            it("should also add these parameters to the subscription request", (done) => {
-                subject.notificationsFor(new ViewModelContext("Admin", "FakePage", {id: 60})).subscribe(_ => {
-                    expect(emitStub.calledWith('subscribe', {
-                        area: "Admin",
-                        viewmodelId: "FakePage",
-                        parameters: {id: 60}
-                    })).to.be(true);
-                    done();
-                });
+            it("should also add these parameters to the subscription request", () => {
+                subject.notificationsFor(new ViewModelContext("Admin", "FakePage", {id: 60}));
+                expect(emitStub.calledWith('subscribe', {
+                    area: "Admin",
+                    viewmodelId: "FakePage",
+                    parameters: {id: 60}
+                })).to.be(true);
             });
         });
     });
 
     context("when a notifications is not needed anymore", () => {
-        it("should dispose the subscription", (done) => {
-            let subscription = subject.notificationsFor(new ViewModelContext("Admin", "FakePage", {id: 60})).subscribe(() => {
-                subscription.dispose();
-                expect(emitStub.calledWith('unsubscribe', {
-                    area: "Admin",
-                    viewmodelId: "FakePage",
-                    parameters: {id: 60}
-                })).to.be(true);
-                done();
+        it("should dispose the subscription", () => {
+            let subscription = subject.notificationsFor(new ViewModelContext("Admin", "FakePage", {id: 60})).subscribe(_ => {
             });
+            subscription.dispose();
+            expect(emitStub.calledWith('unsubscribe', {
+                area: "Admin",
+                viewmodelId: "FakePage",
+                parameters: {id: 60}
+            })).to.be(true);
         });
     });
 });
