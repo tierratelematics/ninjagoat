9835c1c4dc3e89aaed780923de83214cfb186b76
Mattia Franchetto <mt.franchetto@gmail.com>
2016-05-17 11:43:35 +0200

    Fix empty metadata enricher missing annotations

--- scripts/commands/EmptyMetadataEnricher.ts
+++ scripts/commands/EmptyMetadataEnricher.ts
@@ -1,5 +1,7 @@
 import IMetadataEnricher from "./IMetadataEnricher";
+import {injectable} from "inversify";
 
+@injectable()
 class EmptyMetadataEnricher implements IMetadataEnricher {
 
     enrich<T>(metadata?:{[index:string]:any}):{[index:string]:any} {
