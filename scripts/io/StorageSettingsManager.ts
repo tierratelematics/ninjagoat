db6434a6e529f51c3f6b7b4fbc18dc5d6d0a7431
Mattia Franchetto <mt.franchetto@gmail.com>
2016-07-13 15:39:53 +0200

    Add settings manager

---
+++ scripts/io/StorageSettingsManager.ts
@@ -0,0 +1,15 @@
+import ISettingsManager from "./ISettingsManager";
+
+class StorageSettingsManager implements ISettingsManager {
+
+    getValue<T>(key:string, fallback:T):T {
+        return JSON.parse(window.localStorage.getItem(key)) || fallback;
+    }
+
+    setValue<T>(key:string, value:T):void {
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }
+
+}
+
+export default StorageSettingsManager
