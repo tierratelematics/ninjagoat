a4c9f3af1bbae1c6899468b02da14ef18dc8ed72
Mattia Franchetto <mt.franchetto@gmail.com>
2017-03-10 17:59:42 +0100

    Reformat code

--- scripts/net/HttpClient.ts
+++ scripts/net/HttpClient.ts
@@ -9,41 +9,41 @@ import * as _ from "lodash";
 @injectable()
 class HttpClient implements IHttpClient {
 
-    get(url:string, headers?:Dictionary<string>):Rx.Observable<HttpResponse> {
+    get(url: string, headers?: Dictionary<string>): Rx.Observable<HttpResponse> {
         return this.performNetworkCall(url, 'get', undefined, headers);
     }
 
-    post(url:string, body:{}|FormData, headers?:Dictionary<string>):Rx.Observable<HttpResponse> {
+    post(url: string, body: {}|FormData, headers?: Dictionary<string>): Rx.Observable<HttpResponse> {
         return this.performNetworkCall(url, 'post', this.getJsonBody(body), this.addJsonHeaders(headers));
     }
 
-    put(url:string, body:{}, headers?:Dictionary<string>):Rx.Observable<HttpResponse> {
+    put(url: string, body: {}, headers?: Dictionary<string>): Rx.Observable<HttpResponse> {
         return this.performNetworkCall(url, 'put', this.getJsonBody(body), this.addJsonHeaders(headers));
     }
 
-    delete(url:string, headers?:Dictionary<string>):Rx.Observable<HttpResponse> {
+    delete(url: string, headers?: Dictionary<string>): Rx.Observable<HttpResponse> {
         return this.performNetworkCall(url, 'delete', undefined, headers);
     }
 
-    private getJsonBody(body:{}|FormData) {
+    private getJsonBody(body: {}|FormData) {
         return !(body instanceof FormData) ? JSON.stringify(body) : body;
     }
 
-    private addJsonHeaders(headers:Dictionary<string>) {
+    private addJsonHeaders(headers: Dictionary<string>) {
         return _.merge(headers, {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         });
     };
 
-    private performNetworkCall(url:string, method:string, body?:any, headers?:Dictionary<string>):Rx.Observable<HttpResponse> {
+    private performNetworkCall(url: string, method: string, body?: any, headers?: Dictionary<string>): Rx.Observable<HttpResponse> {
         return Rx.Observable.fromPromise(
             window.fetch(url, {
                 method: method,
                 body: body,
                 headers: headers
             }).then<HttpResponse>(response => {
-                let headers:Dictionary<string> = {};
+                let headers: Dictionary<string> = {};
                 response.headers.forEach((value, name) => {
                     headers[name] = value;
                 });
