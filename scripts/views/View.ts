6f9d763e949efce8eb93cb3a5bd9ce8c3f0a3b88
Mattia Franchetto <mt.franchetto@gmail.com>
2017-10-04 11:28:08 +0200

    Add custom props to View in order to allow extension

--- scripts/views/View.ts
+++ scripts/views/View.ts
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-abstract class View<T> extends React.Component<{ viewmodel: T }, {}> {
+abstract class View<T, P = void> extends React.Component<{ viewmodel: T } & P, {}> {
     public viewModel: T = this.props.viewmodel;
 
     abstract render();
